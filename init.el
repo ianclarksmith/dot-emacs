99cdd00731618d21bcdb51e326a2d1fbda82f620
Diego Zamboni <diego@zzamboni.org>
2020-01-12 20:59:23 +0100

    Added erc configuration

--- init.el
+++ init.el
@@ -1169,6 +1169,29 @@
   :load-path ("lisp/keybase-chat")
   :config (require 'keybase))
 
+(use-package erc
+  :custom
+  (erc-autojoin-channels-alist '(("freenode.net" "#elvish" "#hammerspoon"
+                                  "#org-mode")))
+  (erc-autojoin-timing 'ident)
+  (erc-autojoin-delay 5)
+  (erc-fill-function 'erc-fill-static)
+  (erc-fill-static-center 22)
+  (erc-hide-list '("JOIN" "PART" "QUIT"))
+  (erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
+  (erc-lurker-threshold-time 43200)
+  (erc-nick "zzamboni")
+  (erc-prompt-for-nickserv-password nil)
+  (erc-server-reconnect-attempts 5)
+  (erc-server-reconnect-timeout 3)
+  (erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
+                             "324" "329" "332" "333" "353" "477"))
+  :config
+  (add-to-list 'erc-modules 'notifications)
+  (add-to-list 'erc-modules 'spelling)
+  (erc-services-mode 1)
+  (erc-update-modules))
+
 (use-package adoc-mode
   :mode "\\.asciidoc\\'"
   :hook
