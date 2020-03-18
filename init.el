e1ae5af2627a4786389b603754c9d5937ef916cf
Diego Zamboni <diego@zzamboni.org>
2020-03-18 10:53:59 +0100

    More default encoding settings

--- init.el
+++ init.el
@@ -91,6 +91,11 @@
   (customize-set-variable 'url-proxy-services nil))
 
 (set-language-environment "UTF-8")
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
 
 (require 'cl)
 
